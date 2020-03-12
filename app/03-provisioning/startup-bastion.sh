7725612fd12c2a248c93be877ac877307090dff4
Karl Stich <karl-johannes.stich@atos.net>
2020-03-13 00:01:24 +0100

    startup

--- app/03-provisioning/startup-bastion.sh
+++ app/03-provisioning/startup-bastion.sh
@@ -9,7 +9,7 @@ sysctl -w net.ipv4.ip_forward=1
 # Make forwarding persistent.
 sed -i= 's/^[# ]*net.ipv4.ip_forward=[[:digit:]]/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
 
 # Install nginx for instance http health check
 apt-get install -y nginx
