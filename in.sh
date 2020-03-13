eabe536c662161d8c707d841e505441da1323ba2
Karl Stich <karl-johannes.stich@atos.net>
2020-03-13 14:02:57 +0100

    kubespray

--- in.sh
+++ in.sh
@@ -10,6 +10,7 @@ if [ $? -eq 0 ]; then
     
     docker run -it \
         -v $PWD/app:/root/app \
+        -v $PWD/kubespray/inventory/mycluster:/root/kubespray/inventory/mycluster \
         -p 8001:8001 \
         --name k8s-on-gce-tools netactiv/k8s-on-gce-tools:latest
 fi
